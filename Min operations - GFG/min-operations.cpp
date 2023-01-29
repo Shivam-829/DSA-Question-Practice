e606b1e67dc18c431d53ec87c2a6a81677bab703
Shivam Pandey <shivampandey829@outlook.com>
2023-01-29 22:20:46 +0530

    Added solution - LeetHub

--- Min operations - GFG/min-operations.cpp
+++ Min operations - GFG/min-operations.cpp
@@ -8,16 +8,13 @@ class Solution {
   public:
     int solve(int a, int b) {
         // code here
-          if(a==b)
+        if(a==b)
         return 0;
-        
-        if(a==0 || b==0)
+        if(a==0||b==0)
         return 1;
-        
-        int x = a&b;
-        if(a==x || b==x)
+        int x=a&b;
+        if(a==x||b==x)
         return 1;
-        
         return 2;
     }
 };
