3ac1d4211550ae93aad9ac27a7bf36f3aa1b21ef
Shivam Pandey <shivampandey829@outlook.com>
2023-07-18 10:18:03 +0530

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.cpp
+++ Find the number of islands - GFG/find-the-number-of-islands.cpp
@@ -6,59 +6,53 @@ using namespace std;
 class Solution {
   public:
     // Function to find the number of islands.
-   
-        // Code here
-        bool isValid(int i,int j,int n,int m,vector<vector<char>>& grid)
-{
-    if(i>=0&&i<n&&j>=0&&j<m&&grid[i][j]=='1')
+    bool isValid(int i,int j,int n,int m,vector<vector<char>>& grid)
     {
-        return true;
+        if(i>=0&&i<n&&j>=0&&j<m&&grid[i][j]=='1')
+         return true;
+        return false;
     }
-    return false;
-}
-void dfs(int i,int j,int n,int m,vector<vector<char>>& grid)
-{
-    grid[i][j]='0';
-    if(isValid(i+1,j,n,m,grid))
-     dfs(i+1,j,n,m,grid);
-    if(isValid(i-1,j,n,m,grid))
-     dfs(i-1,j,n,m,grid);
-    if(isValid(i,j+1,n,m,grid))
-     dfs(i,j+1,n,m,grid);
-    if(isValid(i,j-1,n,m,grid))
-     dfs(i,j-1,n,m,grid);
-    
-    if(isValid(i-1,j-1,n,m,grid))
-     dfs(i-1,j-1,n,m,grid);
-    if(isValid(i-1,j+1,n,m,grid))
-     dfs(i-1,j+1,n,m,grid);
-    if(isValid(i+1,j+1,n,m,grid))
-     dfs(i+1,j+1,n,m,grid);
-    if(isValid(i+1,j-1,n,m,grid))
-     dfs(i+1,j-1,n,m,grid);
-
-}
-    int numIslands(vector<vector<char>>& grid) 
+    void dfs(int i,int j,int n,int m,vector<vector<char>>& grid)
     {
+        grid[i][j]='0';
+        if(isValid(i+1,j,n,m,grid))
+         dfs(i+1,j,n,m,grid);
+        if(isValid(i-1,j,n,m,grid))
+         dfs(i-1,j,n,m,grid);
+        if(isValid(i,j+1,n,m,grid))
+         dfs(i,j+1,n,m,grid);
+        if(isValid(i,j-1,n,m,grid))
+         dfs(i,j-1,n,m,grid);
+         
+        if(isValid(i-1,j-1,n,m,grid))
+         dfs(i-1,j-1,n,m,grid);
+        if(isValid(i+1,j-1,n,m,grid))
+         dfs(i+1,j-1,n,m,grid);
+        if(isValid(i+1,j+1,n,m,grid))
+         dfs(i+1,j+1,n,m,grid);
+        if(isValid(i-1,j+1,n,m,grid))
+         dfs(i-1,j+1,n,m,grid);
+        
+    }
+    int numIslands(vector<vector<char>>& grid)
+    {
+        // Code here
         int n=grid.size();
         int m=grid[0].size();
         int count=0;
-        vector<bool> vis(n,false);
         for(int i=0;i<n;i++)
         {
+            
             for(int j=0;j<m;j++)
             {
-                if(grid[i][j]=='1')
-                {
-                    count++;
-                 dfs(i,j,n,m,grid);}
+               if(grid[i][j]=='1')
+               {
+                   count++;
+                dfs(i,j,n,m,grid);}
             }
         }
         return count;
-
-        
     }
-    
 };
 
 //{ Driver Code Starts.
